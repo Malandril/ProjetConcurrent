2337cc4337f7841378ed4582b9f2e77d6620fae3
unknown <canava.thomas@gmail.com>
2017-10-22 17:51:37 +0200

    debut des statistiques et analyse des options de lancement

--- lol.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by user on 20/10/2017.
-//
-
--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <pthread.h>
-#include <math.h>
+#include <cmath>
+#include <unistd.h>
+#include <chrono>
 
 #include "Cell.h"
 
-static const int MAX_X = 30;
-static const int MAX_Y = 12;
+using std::cout;
+using std::endl;
+using std::chrono::high_resolution_clock;
+using namespace std::chrono;
+static const int MAX_X = 60;
+static const int MAX_Y = 60;
 static const int DEST_X = -1;
 static const int DEST_Y = -1;
-
 int terrain[MAX_X][MAX_Y];
-
-int nbThread = 20;
-
+int nbThread = 1;
 pthread_mutex_t arrayMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t testMut = PTHREAD_MUTEX_INITIALIZER;
 
 bool isValidCell(int x, int y);
 
@@ -23,6 +27,8 @@ void *computePath(void *pVoid);
 
 void bestCell(int &x, int &y);
 
+void initPersonPos(int *idT, long *posT);
+
 void printTerrain() {
     for (int i = 0; i < MAX_Y; ++i) {
         for (int j = 0; j < MAX_X; ++j) {
@@ -31,46 +37,101 @@ void printTerrain() {
         std::cout << "\n";
     }
     std::cout << std::endl;
-
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    bool metric = false;
+    int opt;
+    while ((opt = getopt(argc, argv, "p::t::m:")) != -1) { //parses arguments and selects relevent ones
+        switch (opt) {
+            case 'p':
+                if (optarg != nullptr) {
+                    nbThread = static_cast<int>(pow(2, atoi(optarg)));
+                }
+                break;
+            case 't':
+                if (optarg != nullptr) {
+                    if (optarg[0] == '1') {
+                        std::cerr << "Not implemented yet" << endl;
+                        exit(-1);
+                    }
+                }
+                break;
+            case 'm':
+                metric = true;
+                break;
+            case '?':
+                cout << optopt << " unknown option " << endl;
+                break;
+            default:
+                break;
+
+        }
+    }
+
     for (int i = 0; i < MAX_Y; ++i) {
         for (int j = 0; j < MAX_X; ++j) {
             terrain[j][i] = 0;
         }
     }
+
     pthread_t tabT[nbThread];
     int idT[nbThread];
     long posT[nbThread];
-
-    for (int i = 0; i < nbThread; i++) {
-
-        long x = rand() % (MAX_X * MAX_Y);
-        while (terrain[x % MAX_X][x / MAX_X]) x = rand() % (MAX_X * MAX_Y);
-        posT[i] = x;
-
-        idT[i] = pthread_create(&tabT[i], nullptr, computePath, (void *) posT[i]);
+    srand(2501); //allows to always have the same position of persons
+    pthread_mutex_lock(&testMut);
+    initPersonPos(idT, posT);
+    high_resolution_clock::time_point start;
+    if(metric) {
+        start = std::chrono::high_resolution_clock::now();
     }
+    for (int j = 0; j < nbThread; ++j) {
+        idT[j] = pthread_create(&tabT[j], nullptr, computePath, (void *) posT[j]);
+    }
+    timespec t = {1};
+    if(!metric) {
+        printTerrain();
+        pthread_mutex_unlock(&testMut);
+    }
+//    while (true) {
+//        printTerrain();
+//        nanosleep(&t, nullptr);
+//    }
     for (int k = 0; k < nbThread; ++k) {
         pthread_join(tabT[k], nullptr);
+    }
+    if(metric) {
+        auto end = high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<milliseconds>(end - start);
+        std::cout << "temps d'execution " << elapsed.count() << " ms" << std::endl;
     }
     return 0;
 
 }
 
+void initPersonPos(int *idT, long *posT) {
+    for (int i = 0; i < nbThread; i++) {
+
+        long x = rand() % (MAX_X * MAX_Y);
+        while (terrain[x % MAX_X][x / MAX_X] || x == 0) x = rand() % (MAX_X * MAX_Y);
+        terrain[x % MAX_X][x / MAX_X] = 1;
+        posT[i] = x;
+    }
+}
+
+
 void *computePath(void *args) {
     int x = ((long) args) % MAX_X;
     int y = ((long) args) / MAX_X;
-
+    pthread_mutex_lock(&testMut);
+    pthread_mutex_unlock(&testMut);
     while (x > 0 || y > 0) {
         pthread_mutex_lock(&arrayMutex); //locking array
         bestCell(x, y);
-        std::cout<<x<<" "<<y<<std::endl;
         pthread_mutex_unlock(&arrayMutex); //unlocking array
     }
     pthread_mutex_lock(&arrayMutex);
-    terrain[x][y]=0;
+    terrain[x][y] = 0;
     pthread_mutex_unlock(&arrayMutex);
 }
 
